53bf226abcc4cc15e07d628441cb1d9a3684243f
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-07-04 11:47:09 -0500

    Added login and register pages

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home(){
   return (
-    <Button>
-    Click me
-    </Button>
+    <UserButton afterSignOutUrl="/" />
   )
 }
