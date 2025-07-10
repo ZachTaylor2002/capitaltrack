7a291b0389a665dbbbfcb66cc2cdd519db9e08ba
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-07-10 14:00:30 -0500

    Added a Welcome message welcoming our users to the Platform

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -67,9 +67,7 @@ export const Navigation = () => {
         </Sheet>
     )
   }
-
   
-
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {routes.map((route) => (
