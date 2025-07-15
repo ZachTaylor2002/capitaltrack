7246f174483a03a11b433cdbf6c9962da01a90dd
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-07-15 16:52:53 -0500

    Account Tab Styling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
 
 export default function Home(){
+  const { onOpen } = useNewAccount();
   return (
-   <div>
-    Home Page
-   </div>
+    <div>
+      <Button onClick={onOpen}>
+        Add an Account
+      </Button>
+    </div>
   );
 }
